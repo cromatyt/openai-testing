34edb3ceab48d7b2a3c3e097c4129ab3b1bbbf91
cromatyt <cromatyt@protonmail.com>
2023-04-19 10:41:28 +0200

    init openai api with python

---
+++ README.md
@@ -0,0 +1,35 @@
+# Openai api with python
+
+## Required
+
+1. Generate key
+
+`https://platform.openai.com/account/api-keys`
+
+2. Create Python venv
+
+```bash
+# Create Python venv
+python3 -m venv venv
+```
+
+```bash
+# Use venv
+source venv/bin/activate
+```
+
+3. Install python packages
+
+```python
+pip install openai
+```
+
+OR
+
+```python
+pip install -r requirements.txt
+```
+
+3. Check documentation
+
+`https://platform.openai.com/docs/api-reference/introduction`
---
+++ testing.py
@@ -0,0 +1,32 @@
+import os
+import openai
+import markdownify
+
+from env import *
+
+openai.organization = OrganizationID
+openai.api_key = APIKeySecret
+#openai.api_key = os.getenv(APIKeySecret)
+
+system_message = input("Enter software name : ")
+system_user = input("Question : ")
+
+print("------------------------------")
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+            "role": "system",
+            "content": system_message
+        },
+        {
+            "role": "user",
+            "content": system_user
+        },
+    ]
+)
+
+response = response["choices"][0]["message"]["content"].replace("'", '"')
+md = markdownify.markdownify(response)
+print(md)
